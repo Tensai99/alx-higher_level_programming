5c2a3a08b6d5cc783591f47cc886659f41b2e294
Tensai99 <eimtingwi@gmail.com>
2023-03-30 09:18:09 +0000

    fix 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -8,18 +7,7 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-#!/usr/bin/python3
 
-"""Define a class Square."""
-
-
-class Square:
-    """Represent a square."""
-
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
@@ -69,3 +57,4 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
